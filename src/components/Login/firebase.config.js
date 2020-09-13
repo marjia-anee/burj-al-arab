cf64aa3de0cfd683dcb46d18011733081a47769a
marjia-anee <marjiazaman@gmail.com>
2020-09-13 21:39:01 +0600

    google sign in done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,13 @@
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyA3aaxyjzs5PwfOSkRtIqLamoaVsNIrfWg",
+    authDomain: "burj-al-arab-a1ec8.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-a1ec8.firebaseio.com",
+    projectId: "burj-al-arab-a1ec8",
+    storageBucket: "burj-al-arab-a1ec8.appspot.com",
+    messagingSenderId: "165221319812",
+    appId: "1:165221319812:web:713039ee7c2a8b7923aed1"
+  };
+
+  export default firebaseConfig;
+ 
